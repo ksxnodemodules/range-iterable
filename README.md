eec57c6581c5285fa7d1e6abf69e03b50fa4cdd6
khai96_ <hvksmr1996@gmail.com>
2016-06-08 11:40:17 +0700

    first born :birthday:

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# range-iterable
+
+# Requirements
+
+ * Node >= 6.0.0
+
+# Features
+
+ * Python-like `range` class for JavaScript
+
+# Usage
+
+## Import
+
+```javascript
+var Range = require('range-iterable'); // `Range` is a constructor, always takes 2 arguments
+var {range} = Range; // `range` is a function, takes 1 or 2 arguments
+```
+
+## Basic usages
+
+```javascript
+var x0 = [...new Range(3, 7)];
+var x1 = [...range(2, 5)];
+var x2 = [...range(5)];
+var x3 = [...range(5).map(String)];
+console.log({x0, x1, x2, x3});
+```
+
+## Extra methods
+
+```javascript
+var x0 = range(5).reverse();
+var x1 = range(5).shift(2);
+var x2 = range(5).multiply(3);
+var x4 = range(5).reverse().shift(2).multiply(3);
+console.log({x0, x1, x2, x3});
+```
---
+++ index.js
@@ -0,0 +1,71 @@
+
+((module) => {
+	'use strict';
+
+	var {XIterable, Root} = require('x-iterable-base');
+
+	var {iterator} = Symbol;
+
+	var createYielder = (base, callback) => {
+		var iterable = base.map(callback);
+		return {
+			[iterator]: () => iterable[iterator](),
+			__proto__: base
+		};
+	};
+
+	class RangeIterable extends XIterable(Root) {
+
+		constructor(begin, end) {
+
+			super();
+
+			begin >>= 0;
+			end >>= 0;
+
+			return begin <= end && {
+
+				* [iterator]() {
+					for (let i = begin; i != end; ++i) {
+						yield i;
+					}
+				},
+
+				reverse() {
+					var end_s1 = end - 1;
+					return createYielder(this, value => end_s1 - value);
+				},
+
+				__proto__: this
+
+			};
+
+		}
+
+		* [iterator]() {}
+
+		shift(delta) {
+			return createYielder(this, value => value + delta);
+		}
+
+		multiply(factor) {
+			return createYielder(this, value => value * factor);
+		}
+
+		static range(...args) {
+			switch (args.length) {
+				case 1:
+					return new Range(0, args[0]);
+				case 2:
+					return new Range(...args);
+			}
+			throw new RangeError('Number of provided arguments must be 1 or 2');
+		}
+
+	}
+
+	class Range extends RangeIterable {}
+
+	module.exports = Range;
+
+})(module);
